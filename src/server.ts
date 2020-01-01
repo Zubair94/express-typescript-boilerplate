38c7af8a5f30dd40f55803b75f91e8d58fd5696d
Fahim Uddin <raydvard@gmail.com>
2020-01-01 14:23:50 +0600

    updated some tsconfig

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 import app from "./app";
-import {PORT} from "./config";
+import { PORT } from "./config";
 const port = PORT ? PORT : 3000;
 app.listen(port, () => {
     console.log('Express server listening on PORT ' + port);
